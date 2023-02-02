69468e327ae723194707bc00710d3ae7471740d5
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-02-02 23:57:22 +0100

    User authentication for current users sign_in

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_225146) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_204844) do
   create_table "tweeets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
