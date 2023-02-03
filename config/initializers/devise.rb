f5cc144b607f2ce277d9f4e28d0776e5af2fc1d6
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-02-03 23:56:44 +0100

    Add user authentication to add tweets by a user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -213,6 +213,7 @@ Devise.setup do |config|
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
 
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
   # Warn on the last attempt before the account is locked.
   # config.last_attempt_warning = true
 
